0c994695369d0d33434ccaa995addb5c797e56d6
Eady <edith.yelf@sky.uk>
2020-03-30 14:46:00 +0100

    add lite-server

---
+++ app.js
@@ -0,0 +1,13 @@
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 1;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'result is: ';
+add(number1, number2, printResult, resultPhrase);
